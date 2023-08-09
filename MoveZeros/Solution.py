750e7ec7feb6c9b046dadf5793c145f6c93454db
cath0806 <96349137+cath0806@users.noreply.github.com>
2023-08-09 14:27:18 +0530

    Create Solution.py

---
+++ MoveZeros/Solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        i=0
+        c=0
+        while(c<len(nums)):
+            if(nums[i]==0): 
+                nums.pop(i)
+                nums.append(0)
+            else:
+                i=i+1
+            c=c+1
